077b1df6f7a12e336127176e9dd81103274b0e3e
xvr6 <xvr0612@gmail.com>
2026-07-15 20:49:13 -0400

    recipe tweaks, mainly cake and other food stuffs

--- packs/p3d/kubejs/startup_scripts/recipeModifications/additions.js
+++ packs/p3d/kubejs/startup_scripts/recipeModifications/additions.js
@@ -1,3 +1,35 @@
 global.additions = (event) => {
-  console.log(" additions run!");
+  //replaces removed cinnamon recipe eith expanded delight
+  event.shapeless(
+    Item.of("creategarnished:gingerbread_flour", 2), //output
+    [
+      "creategarnished:ginger_root",
+      "creategarnished:pine_nut_flour",
+      "2x expandeddelight:cinnamon",
+    ],
+  );
+
+  // - Readd cake recipes
+  event.recipes.createFilling("createaddition:chocolate_cake", [
+    "ratatouille:cake_base",
+    Fluid.of("create:chocolate", 500),
+  ]);
+  // add in cake bases
+  event.recipes.createCompacting(
+    Item.of("create_enchantment_industry:experience_cake_base", 4),
+    "ratatouille:cake_base",
+    "minecraft:lapis_lazuli",
+  );
+  event.recipes.createCompacting(
+    Item.of("create:blaze_cake_base", 4),
+    "ratatouille:cake_base",
+    "create:cinder_flour",
+  );
+  event.recipes
+    .createCompacting(
+      Item.of("create_deep_dark:echo_cake_base", 4),
+      "ratatouille:cake_base",
+      "create_deep_dark:sculk_flour",
+    )
+    .heated();
 };
--- packs/p3d/kubejs/startup_scripts/recipeModifications/removals.js
+++ packs/p3d/kubejs/startup_scripts/recipeModifications/removals.js
@@ -1,6 +1,29 @@
 global.removals = (event) => {
-  //removing duplicate cinnamon bark
+  // - Removing duplicate cinnamon bark
   event.remove({ output: "creategarnished:cinnamon_bark" });
-  //event.remove({ id: "creategarnished:crafting/gingerbread_flour" });
-  //todo: work on changing recipies for pancakes, cakes to use ratatouille cake bases instead.
+  event.remove({ output: "creategarnished:cinnamon_stick" });
+  event.remove({ id: "creategarnished:crafting/gingerbread_flour" });
+
+  // - Removing all pasta recipes but garnished
+  event.remove({
+    not: { type: "ratatouille:squeezing" },
+    output: "farmersdelight:raw_pasta",
+  });
+
+  // - Remove any recipes for chocolate that dont use the mold
+  event.remove({
+    not: { type: "ratatouille:demolding" },
+    output: "create:bar_of_chocolate",
+  });
+  // no longer has ANY purpose so can be removed
+  event.remove({ output: "createdieselgenerators:mold" });
+
+  // - Remove recipies for cakes that do not use the cake base from ratatouille
+  event.remove({ output: "createaddition:cake_base" });
+  event.remove({ input: "createaddition:cake_base" });
+  event.remove({ input: "createaddition:cake_base_baked" });
+  //evil....... :)
+  event.remove({ output: "create_deep_dark:echo_cake_base" });
+  event.remove({ output: "create:blaze_cake_base" });
+  event.remove({ output: "create_enchantment_industry:experience_cake_base" });
 };
