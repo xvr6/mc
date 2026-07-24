f808dec2b6811db57b24ac92b5d5c2c13f4afa0a
xvr6 <xvr0612@gmail.com>
2026-07-24 16:04:28 -0400

    item modification scripts moved

--- packs/p3d/kubejs/server_scripts/recipeModification/cobblemon.js
+++ packs/p3d/kubejs/server_scripts/recipeModification/cobblemon.js
@@ -26,6 +26,40 @@ ItemEvents.modifyTooltips((event) => {
   );
 });
 
+ItemEvents.modification((event) => {
+  event.modify("cobblemonsizevariation:huge_essence", (item) => {
+    item.foodProperties = (food) => {
+      food.alwaysEdible();
+      food.fastToEat();
+    };
+  });
+  event.modify("cobblemonsizevariation:growth_essence", (item) => {
+    item.foodProperties = (food) => {
+      food.alwaysEdible();
+      food.fastToEat();
+    };
+  });
+  event.modify("cobblemonsizevariation:normal_essence", (item) => {
+    item.foodProperties = (food) => {
+      food.alwaysEdible();
+      food.fastToEat();
+    };
+  });
+  event.modify("cobblemonsizevariation:shrink_essence", (item) => {
+    item.foodProperties = (food) => {
+      food.alwaysEdible();
+      food.fastToEat();
+    };
+  });
+
+  event.modify("cobblemonsizevariation:tiny_essence", (item) => {
+    item.foodProperties = (food) => {
+      food.alwaysEdible();
+      food.fastToEat();
+    };
+  });
+});
+
 ServerEvents.recipes((event) => {
   // huge -> growth
   event.shapeless(
--- packs/p3d/kubejs/startup_scripts/sizeItems.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry("item", (event) => {
-  event.modify("cobblemonsizevariation:huge_essence").alwaysEdible(true);
-  event.modify("cobblemonsizevariation:growth_essence").alwaysEdible(true);
-  event.modify("cobblemonsizevariation:normal_essence").alwaysEdible(true);
-  event.modify("cobblemonsizevariation:shrink_essence").alwaysEdible(true);
-  event.modify("cobblemonsizevariation:tiny_essence").alwaysEdible(true);
-});
