291575b190c3f1d6fbf54e403ffa92d9cc47f22f
xvr6 <xvr0612@gmail.com>
2026-07-24 15:51:47 -0400

    script error oopsie

--- packs/p3d/kubejs/server_scripts/recipeModification/cobblemon.js
+++ packs/p3d/kubejs/server_scripts/recipeModification/cobblemon.js
@@ -1,3 +1,31 @@
+ItemEvents.modifyTooltips((event) => {
+  event.add(
+    "cobblemonsizevariation:huge_essence",
+    { shift: false },
+    Text.gray("Eat to become CHONKY"),
+  );
+  event.add(
+    "cobblemonsizevariation:growth_essence",
+    { shift: false },
+    Text.gray("Eat to become larger"),
+  );
+  event.add(
+    "cobblemonsizevariation:normal_essence",
+    { shift: false },
+    Text.gray("Eat to return to normal size"),
+  );
+  event.add(
+    "cobblemonsizevariation:shrink_essence",
+    { shift: false },
+    Text.gray("Eat to become smaller"),
+  );
+  event.add(
+    "cobblemonsizevariation:tiny_essence",
+    { shift: false },
+    Text.gray("Eat to become MINISCULE"),
+  );
+});
+
 ServerEvents.recipes((event) => {
   // huge -> growth
   event.shapeless(
--- packs/p3d/kubejs/startup_scripts/sizeItems.js
+++ packs/p3d/kubejs/startup_scripts/sizeItems.js
@@ -5,31 +5,3 @@ StartupEvents.registry("item", (event) => {
   event.modify("cobblemonsizevariation:shrink_essence").alwaysEdible(true);
   event.modify("cobblemonsizevariation:tiny_essence").alwaysEdible(true);
 });
-
-ItemEvents.modifyTooltips((event) => {
-  event.add(
-    "cobblemonsizevariation:huge_essence",
-    { shift: false },
-    Text.gray("Eat to become CHONKY"),
-  );
-  event.add(
-    "cobblemonsizevariation:growth_essence",
-    { shift: false },
-    Text.gray("Eat to become larger"),
-  );
-  event.add(
-    "cobblemonsizevariation:normal_essence",
-    { shift: false },
-    Text.gray("Eat to return to normal size"),
-  );
-  event.add(
-    "cobblemonsizevariation:shrink_essence",
-    { shift: false },
-    Text.gray("Eat to become smaller"),
-  );
-  event.add(
-    "cobblemonsizevariation:tiny_essence",
-    { shift: false },
-    Text.gray("Eat to become MINISCULE"),
-  );
-});
