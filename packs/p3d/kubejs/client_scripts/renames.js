00c8487ef435cd010d93be57d15ac1ef1b80310e
xvr6 <xvr0612@gmail.com>
2026-07-25 15:39:40 -0400

    more lang fixes

--- packs/p3d/kubejs/client_scripts/renames.js
+++ packs/p3d/kubejs/client_scripts/renames.js
@@ -1,18 +1,12 @@
 ClientEvents.lang("en_us", (event) => {
+  event.renameBlock("ratatouille_fried_delights:sunflower_oil", "Cooking Oil");
+  event.renameItem("ratatouille_fried_delights:sunflower_oil", "Cooking Oil");
   event.renameItem(
-    "block.ratatouille_fried_delights.sunflower_oil",
-    "Cooking Oil",
-  );
-  event.renameItem(
-    "fluid.ratatouille_fried_delights.sunflower_oil",
-    "Cooking Oil",
-  );
-  event.renameItem(
-    "item.ratatouille_fried_delights.sunflower_oil_bucket",
+    "ratatouille_fried_delights:sunflower_oil_bucket",
     "Cooking Oil Bucket",
   );
   event.renameItem(
-    "item.ratatouille_fried_delights.sunflower_seed_oil_bottle",
+    "ratatouille_fried_delights:sunflower_seed_oil_bottle",
     "Cooking Oil Bottle",
   );
   event.renameItem("item.rusticdelight.cooking_oil", "Bottled Cooking Oil");
