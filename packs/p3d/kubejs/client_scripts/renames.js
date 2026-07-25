a0ece713e5f74947afded31e5e2c5d2f1e42a1cb
xvr6 <xvr0612@gmail.com>
2026-07-25 15:43:12 -0400

    lang

--- packs/p3d/kubejs/client_scripts/renames.js
+++ packs/p3d/kubejs/client_scripts/renames.js
@@ -1,6 +1,5 @@
 ClientEvents.lang("en_us", (event) => {
   event.renameBlock("ratatouille_fried_delights:sunflower_oil", "Cooking Oil");
-  event.renameItem("ratatouille_fried_delights:sunflower_oil", "Cooking Oil");
   event.renameItem(
     "ratatouille_fried_delights:sunflower_oil_bucket",
     "Cooking Oil Bucket",
