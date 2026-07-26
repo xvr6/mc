20056c15a3493deddf231ea9b1421e7affaa5412
xvr6 <xvr0612@gmail.com>
2026-07-26 17:28:45 -0400

    vibeslop fix? idk it might be nonfunctional until i can fork and debug

--- packs/p3d/kubejs/startup_scripts/newfan.js
+++ packs/p3d/kubejs/startup_scripts/newfan.js
@@ -6,7 +6,6 @@ CreateMoreFansEvents.registry((event) => {
   event
     .create(`poinky:roosting`)
     .setCatalystBlockTag(`poinky:roosting_catalyst`)
-    .setProcessingParticles($ParticleTypes.CHERRY_LEAVES)
     .displayName("Bulk Roosting")
     .setJeiCategoryDisplayItem(`create_integrated_farming:chicken_roost`)
     .setJeiAttachedBlock(`create_integrated_farming:chicken_roost`);
