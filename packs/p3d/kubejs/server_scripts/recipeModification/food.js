8bd131e01f51269c02336a85b0c7c9063e59b09a
xvr6 <xvr0612@gmail.com>
2026-07-26 17:57:41 -0400

    remove hanging recipe

--- packs/p3d/kubejs/server_scripts/recipeModification/food.js
+++ packs/p3d/kubejs/server_scripts/recipeModification/food.js
@@ -173,9 +173,4 @@ ServerEvents.recipes((event) => {
       ["ratatouille:boil_stone", Fluid.of(`createaddition:seed_oil`, 500)],
     )
     .heated();
-  event.custom({
-    type: "poinky:roosting",
-    ingredients: [{ item: "ratatouille:egg_shell" }],
-    results: [{ id: "minecraft:egg" }],
-  });
 });
