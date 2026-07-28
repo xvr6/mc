a8ee7141cc238c814c432bce27d22f6f26e69993
xvr6 <xvr0612@gmail.com>
2026-07-28 19:29:41 -0400

    removal of origins

--- packs/p3d/kubejs/server_scripts/sizeMod.js
+++ packs/p3d/kubejs/server_scripts/sizeMod.js
@@ -1,20 +1,19 @@
-const changeMon = (player, amount) => {
-  console.log("hai");
-  const pNbt = player.getNbt();
-  const activeSlot = pNbt.get("SynchroSavedTeam").getInt("active");
-  console.log(activeSlot);
-  const currentSize = pNbt
-    .get("SynchroSavedTeam")
-    .get(`slot_${activeSlot}`)
-    .getFloat("ScaleModifier");
-  console.log(currentSize);
-  pNbt["SynchroSavedTeam"][`slot_${activeSlot}`]["ScaleModifier"] =
-    currentSize + amount;
-  player.server.runCommandSilent(
-    `/synchro slot activate ${activeSlot + 1} ${player.getUsername()}`,
-  );
-  console.log("scaled mon");
-};
+//const changeMon = (player, amount) => {
+//const pNbt = player.getNbt();
+//const activeSlot = pNbt.get("SynchroSavedTeam").getInt("active");
+//console.log(activeSlot);
+//const currentSize = pNbt
+//  .get("SynchroSavedTeam")
+//  .get(`slot_${activeSlot}`)
+//  .getFloat("ScaleModifier");
+//console.log(currentSize);
+//pNbt["SynchroSavedTeam"][`slot_${activeSlot}`]["ScaleModifier"] =
+//  currentSize + amount;
+//player.server.runCommandSilent(
+//  `/synchro slot activate ${activeSlot + 1} ${player.getUsername()}`,
+//);
+//console.log("scaled mon");
+//};
 
 ItemEvents.modifyTooltips((event) => {
   event.add(
