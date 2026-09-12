22f0bb93f4fd81cece796d6756435f37f2e30937
xvr6 <xvr0612@gmail.com>
2026-09-12 12:42:29 -0400

    kjs fix

--- packs/p3d/kubejs/server_scripts/recipeModification/tags.js
+++ packs/p3d/kubejs/server_scripts/recipeModification/tags.js
@@ -58,7 +58,6 @@ ServerEvents.tags("item", (event) => {
     "poinky:removed",
     "#dndecor:containers_decor",
     "#createdeco:shipping_containers", // block tag, not item tag; must be removed by hand
-    "create_connected:item_silo",
     "create_sa:vault_component",
   );
   Color.DYE.forEach((color) => {
