f15c7618efc8d380be1ab295dca661cd130b0286
xvr6 <xvr0612@gmail.com>
2026-09-25 19:21:52 -0400

    hanging kjs

--- packs/p32d/kubejs/server_scripts/sizeMod.js
+++ packs/p32d/kubejs/server_scripts/sizeMod.js
@@ -1,20 +1,3 @@
-//const changeMon = (player, amount) => {
-//const pNbt = player.getNbt();
-//const activeSlot = pNbt.get("SynchroSavedTeam").getInt("active");
-//console.log(activeSlot);
-//const currentSize = pNbt
-//  .get("SynchroSavedTeam")
-//  .get(`slot_${activeSlot}`)
-//  .getFloat("ScaleModifier");
-//console.log(currentSize);
-//pNbt["SynchroSavedTeam"][`slot_${activeSlot}`]["ScaleModifier"] =
-//  currentSize + amount;
-//player.server.runCommandSilent(
-//  `/synchro slot activate ${activeSlot + 1} ${player.getUsername()}`,
-//);
-//console.log("scaled mon");
-//};
-
 ItemEvents.modifyTooltips((event) => {
   event.add(
     "rusticdelight:cooking_oil",
@@ -36,31 +19,5 @@ ItemEvents.modifyTooltips((event) => {
     Ingredient.of("#poinky:removed"),
     { shift: false },
     Text.red("Recipe Removed"),
-  );
-
-  event.add(
-    "cobblemonsizevariation:huge_essence",
-    { shift: false },
-    Text.gray("WIP: Eat to become CHONKY"),
-  );
-  event.add(
-    "cobblemonsizevariation:growth_essence",
-    { shift: false },
-    Text.gray("WIP: Eat to become larger"),
-  );
-  event.add(
-    "cobblemonsizevariation:normal_essence",
-    { shift: false },
-    Text.gray("WIP: Eat to return to normal size"),
-  );
-  event.add(
-    "cobblemonsizevariation:shrink_essence",
-    { shift: false },
-    Text.gray("WIP: Eat to become smaller"),
-  );
-  event.add(
-    "cobblemonsizevariation:tiny_essence",
-    { shift: false },
-    Text.gray("WIP: Eat to become MINISCULE"),
   );
 });
